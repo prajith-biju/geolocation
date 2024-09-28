b51e5fae9d3c247cccb5d066e6b8a3fbc41486ce
Prajith Biju <prajithbiju05@gmail.com>
2024-09-28 17:49:53 +0530

    added country

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import { geolocation } from '@vercel/functions';
 import { NextRequest } from 'next/server';
  
 export default function GET(request: NextRequest) {
-  const { city } = geolocation(request);
+  const { city, country, countryRegion } = geolocation(request);
   console.log(city)
-  return new Response(`<h1>Your location is ${city}</h1>`, {
+  return new Response(`<h1>Your location is ${city} --- ${country} --- ${countryRegion}</h1>`, {
     headers: { 'content-type': 'text/html' },
   });
 
