6b46e9fc349a39ac4e432f8dd6fa8ed6f71c9496
Prajith Biju <prajithbiju05@gmail.com>
2024-09-28 17:43:49 +0530

    reoved

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { geolocation } from '@vercel/functions';
-import { NextResponse, NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
  
 export default function GET(request: NextRequest) {
   const { city } = geolocation(request);
