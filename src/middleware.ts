f403cc43e8874a8f7fc090663e852ef2fb9d15d4
Prajith Biju <prajithbiju05@gmail.com>
2024-09-28 17:35:37 +0530

    change result

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,18 @@ import { NextResponse, NextRequest } from 'next/server';
 export default function GET(request: NextRequest) {
   const { city } = geolocation(request);
   console.log(city)
-  // return new Response(`<h1>Your location is ${city}</h1>`, {
-  //   headers: { 'content-type': 'text/html' },
-  // });
+  return new Response(`<h1>Your location is ${city}</h1>`, {
+    headers: { 'content-type': 'text/html' },
+  });
 
 
   // NextResponse.redirect(new URL(`${city}`, request.url))
 
   // return NextResponse.rewrite(new URL('/dest', request.url))
 
-  const url = request.nextUrl.clone()
-  url.pathname = '/dest'
-  return NextResponse.redirect(url)
+  // const url = request.nextUrl.clone()
+  // url.pathname = '/dest'
+  // return NextResponse.redirect(url)
 }
 
 // export const config = {
